ed7d475481223c5f23924d1a195cf667f1190aff
geversonfernandes <gever.fernandes@gmail.com>
2024-01-22 18:48:40 -0300

    feat: add articlesController and categoriesController

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 const connection = require('./database/database')
+const categoriesController = require('./categories/categoriesController')
+const articlesController = require('./articles/articlesController')
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
-
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use('/', categoriesController)
+app.use('/', articlesController)
 
 app.get('/', (req, res) => {
   res.render('index')
