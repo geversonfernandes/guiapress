6a20f4f9192f3b489a3ccbb6dd72bd03c9514abc
geversonfernandes <gever.fernandes@gmail.com>
2024-01-25 07:40:59 -0300

    feat: add category registration route

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('./Category')
+const slugify = require('slugify')
 
 router.get('/admin/categories/new', (req, res) => {
   res.render('admin/categories/new')
+})
+
+router.post('/categories/save', (req, res) => {
+  const title = req.body.title
+  if (title != undefined) {
+    Category.create({
+      title,
+      slug: slugify(title),
+    }).then(() => {
+      res.redirect('/')
+    })
+  } else {
+    res.redirect('/admin/categories/new')
+  }
 })
 
 module.exports = router
