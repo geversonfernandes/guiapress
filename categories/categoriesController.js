6a7d2f6a9f8ad3d69de7b4bcffc3145adf4e3a8b
geversonfernandes <gever.fernandes@gmail.com>
2024-01-31 10:13:59 -0300

    feat: add route and delete categories function

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -27,4 +27,23 @@ router.get('/admin/categories', (req, res) => {
   })
 })
 
+router.post('/categories/delete', (req, res) => {
+  const id = req.body.id
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy({
+        where: {
+          id,
+        },
+      }).then(() => {
+        res.redirect('/admin/categories')
+      })
+    } else {
+      res.redirect('/admin/categories')
+    }
+  } else {
+    res.redirect('/admin/categories')
+  }
+})
+
 module.exports = router
